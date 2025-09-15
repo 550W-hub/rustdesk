30b5aadf0ce3d15cf6a8b14de30a9e93665808e4
550W-hub <jkchen.chn@gmail.com>
2025-09-15 12:21:50 +0800

    修改替换 API 地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "120.26.247.48".to_owned()
 }
 
 #[inline]
